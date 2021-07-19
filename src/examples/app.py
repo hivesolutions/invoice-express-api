a04b65da79a58d6a69968bc9bef7757c74c72c30
João Magalhães <joamag@gmail.com>
2021-07-19 18:09:00 +0100

    feat: support for create and finalize invoice

--- src/examples/app.py
+++ src/examples/app.py
@@ -24,6 +24,34 @@ class InvoiceExpressApp(appier.WebApp):
         invoices = api.list_invoices()
         return invoices
 
+    @appier.route("/invoices/create", "GET")
+    def create_invoice(self):
+        finalize = self.field("finalize", True, cast = bool)
+        pdf = self.field("pdf", False, cast = bool)
+        api = self.get_api()
+        invoice = api.create_invoice(
+            dict(
+                date = "01/01/2023",
+                due_date = "01/02/2023",
+                client = dict(name = "DUMMY", code = "DUM"),
+                items = [
+                    dict(
+                        name = "BAR",
+                        description = "Chocolate Bar",
+                        unit_price = "123,0000",
+                        quantity = "1"
+                    )
+                ]
+            )
+        )
+        invoice_id = invoice["invoice"]["id"]
+        if finalize: api.finalize_invoice(invoice_id)
+        invoice = api.get_invoice(invoice_id)
+        if pdf:
+            pdf_invoice = api.pdf_invoice(invoice_id)
+            return self.redirect(pdf_invoice["output"]["pdfUrl"])
+        return invoice
+
     @appier.route("/invoices/<str:id>", "GET")
     def invoice(self, id):
         api = self.get_api()
--- src/invoice_express/invoice.py
+++ src/invoice_express/invoice.py
@@ -8,7 +8,30 @@ class InvoiceAPI(object):
         contents = self.get(url, **kwargs)
         return contents
 
+    def create_invoice(self, invoice):
+        url = self.base_url + "invoices.json"
+        contents = self.post(url, data_j = dict(invoice = invoice))
+        return contents
+
     def get_invoice(self, id):
         url = self.base_url + "invoices/%s.json" % id
+        contents = self.get(url)
+        return contents
+
+    def finalize_invoice(self, id, message = "Finalized"):
+        url = self.base_url + "invoices/%s/change-state.json" % id
+        contents = self.post(
+            url,
+            data_j = dict(
+                invoice = dict(
+                    state = "finalized",
+                    message = message
+                )
+            )
+        )
+        return contents
+
+    def pdf_invoice(self, id):
+        url = self.base_url + "api/pdf/%s.json" % id
         contents = self.get(url)
         return contents
